d5162db0ddffa7f423f0a5deb4b3c85b7db0dec8
nithish-nithin <smartnithi2002@gmail.com>
2022-06-30 16:50:40 +0530

    n

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pro from "./Component/PRO/Pro";
+
+function App() {
+  return (
+    <div>
+      <Pro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/PRO/Pro.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Pro.css";
+
+function Pro() {
+  return (
+    <div>
+      <section className="sec1-container">
+        <div className="content">
+          <div className="sec1-head">
+            Extremely intellectual & <br />
+            <span id="sec1-span">challenging Projects</span>
+          </div>
+          <div className="sec1-para">
+            Our ultimate moto is fulfill the demand of <br />
+            the buyers by making them satisfied with <br />
+            proper file delivery.
+          </div>
+        </div>
+
+        <div className="asset-right"></div>
+      </section>
+    </div>
+  );
+}
+
+export default Pro;
